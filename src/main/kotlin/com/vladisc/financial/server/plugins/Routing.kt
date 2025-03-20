c6e7c866a9525419d118a16cb5c36b7f38a6e0b8
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2025-03-20 18:23:32 +0200

    trying out header and status, getting params

--- src/main/kotlin/com/vladisc/financial/server/plugins/Routing.kt
+++ src/main/kotlin/com/vladisc/financial/server/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.vladisc.financial.server.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -16,6 +17,15 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello, my name is Vlad")
+        }
+        get("/users/{userId}") {
+            val userId = call.parameters["userId"]
+            val header = call.request.headers["Connection"]
+            if (userId?.toIntOrNull() == 1) {
+                call.response.header("CustomHeader", userId)
+                call.response.status(HttpStatusCode(201, "Ok"))
+            }
+            call.respondText("Hello, $userId with $header")
         }
     }
 }
