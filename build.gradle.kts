fc67d44c1a6c6a7e87a70602d7b324dfd62d620a
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2025-03-20 14:56:01 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    application
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "com.vladisc.financial.server"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+    implementation("io.ktor:ktor-server-core:3.1.0")
+    implementation("io.ktor:ktor-server-netty:3.1.0")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(23)
+}
---
+++ src/main/kotlin/com/vladisc/financial/server/Application.kt
@@ -0,0 +1,15 @@
+package com.vladisc.financial.server
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello, my name is Vlad")
+        }
+    }
+}
