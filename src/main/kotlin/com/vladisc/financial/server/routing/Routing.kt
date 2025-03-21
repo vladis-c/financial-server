245d39004af628a9554bfcfd9eac38343cbd2706
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2025-03-21 14:48:41 +0200

    Organised functions: now on signup user get the token also. Refresh token generation

--- src/main/kotlin/com/vladisc/financial/server/routing/Routing.kt
+++ src/main/kotlin/com/vladisc/financial/server/routing/Routing.kt
@@ -14,9 +14,13 @@ import org.mindrot.jbcrypt.BCrypt
 import java.util.Date
 
 private const val oneWeekInMillis = 7 * 24 * 60 * 60 * 1000L
+private const val refreshTokenExpiry = oneWeekInMillis * 4
 
 @Serializable
 data class UserCredentials(val username: String, val password: String)
+
+@Serializable
+data class TokenResponse(val accessToken: String, val refreshToken: String)
 
 fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudience: String, jwtSecret: String) {
     route("/auth") {
@@ -25,7 +29,10 @@ fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudie
             val user = UserCredentials(credentials.username, credentials.password)
             val success = userRepository.addUser(user.username, user.password)
             if (success) {
-                call.respond(user)
+                val userRow = userRepository.findUser(user.username)
+                val tokenResponse = generateTokens(userRow?.get(Users.id), jwtIssuer, jwtAudience, jwtSecret, Date(System.currentTimeMillis() + oneWeekInMillis), Date(System.currentTimeMillis() + refreshTokenExpiry))
+                callTokenHeader(call, tokenResponse.accessToken, GMTDate(System.currentTimeMillis() + oneWeekInMillis))
+                call.respond(HttpStatusCode.Created, tokenResponse)
             } else {
                 call.respond(HttpStatusCode.Conflict, "Username already exists")
             }
@@ -49,22 +56,9 @@ fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudie
             }
 
             val expirationDate = Date(System.currentTimeMillis() + oneWeekInMillis)
-            val token = JWT.create()
-                .withIssuer(jwtIssuer)
-                .withAudience(jwtAudience)
-                .withClaim("userId", userRow[Users.id])
-                .withExpiresAt(expirationDate)
-                .sign(Algorithm.HMAC256(jwtSecret))
-
-            call.response.cookies.append(
-                "sessionid",
-                token,
-                expires = GMTDate(System.currentTimeMillis() + oneWeekInMillis),
-                maxAge = oneWeekInMillis,
-                httpOnly = true,
-                secure = true,
-                extensions = mapOf("SameSite" to "Lax")
-            )
+            val tokenResponse = generateTokens(userRow[Users.id], jwtIssuer, jwtAudience, jwtSecret, expirationDate, Date(System.currentTimeMillis() + refreshTokenExpiry))
+            callTokenHeader(call, tokenResponse.accessToken, GMTDate(System.currentTimeMillis() + oneWeekInMillis))
+            call.respond(HttpStatusCode.Created, tokenResponse)
 
             call.response.header(HttpHeaders.Expires, expirationDate.time.toString())
             call.respond(HttpStatusCode.OK)
@@ -72,4 +66,33 @@ fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudie
     }
 }
 
+fun generateTokens(userId: Int?, jwtIssuer: String, jwtAudience: String, jwtSecret: String,expirationDate: Date, refreshExpirationDate: Date): TokenResponse {
+    val accessToken = JWT.create()
+        .withIssuer(jwtIssuer)
+        .withAudience(jwtAudience)
+        .withClaim("userId", userId)
+        .withExpiresAt(expirationDate)
+        .sign(Algorithm.HMAC256(jwtSecret))
+
+    val refreshToken = JWT.create()
+        .withIssuer(jwtIssuer)
+        .withAudience(jwtAudience)
+        .withClaim("userId", userId)
+        .withExpiresAt(refreshExpirationDate)
+        .sign(Algorithm.HMAC256(jwtSecret))
+
+    return TokenResponse(accessToken, refreshToken)
+}
+
+fun callTokenHeader(call: RoutingCall, token: String, expires: GMTDate) {
+    call.response.cookies.append(
+        "sessionid",
+        token,
+        expires = expires,
+        maxAge = oneWeekInMillis,
+        httpOnly = true,
+        secure = true,
+        extensions = mapOf("SameSite" to "Lax")
+    )
+}
 
