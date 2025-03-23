33ce906aabbdfbe9b2558d1cd4dcecfc81fd6a08
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2025-03-23 15:28:12 +0200

    Put all auth routing util functions to separate file

--- src/main/kotlin/com/vladisc/financial/server/routing/AuthRouting.kt
+++ src/main/kotlin/com/vladisc/financial/server/routing/AuthRouting.kt
@@ -1,7 +1,5 @@
 package com.vladisc.financial.server.routing
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
 import com.vladisc.financial.server.models.ErrorRouting
 import com.vladisc.financial.server.models.ErrorRoutingStatus
 import com.vladisc.financial.server.models.Users
@@ -10,32 +8,14 @@ import io.ktor.http.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.Serializable
 import org.mindrot.jbcrypt.BCrypt
-import java.text.SimpleDateFormat
 import java.util.Date
-import java.util.Locale
-import java.util.TimeZone
-
-private const val oneWeekInMillis = 7 * 24 * 60 * 60 * 1000L
-private const val fourWeeksInMillis = oneWeekInMillis * 4
-
-private const val USER_CLAIM = "uid"
-
-private val accessTokenExpiryDate = System.currentTimeMillis() + oneWeekInMillis
-private val refreshTokenExpiryDate = System.currentTimeMillis() + fourWeeksInMillis
-
-@Serializable
-data class UserCredentials(val username: String, val password: String)
-
-@Serializable
-data class TokenResponse(val accessToken: String, val refreshToken: String?)
 
 fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudience: String, jwtSecret: String) {
     route("/auth") {
         post("/signup") {
             try {
-                val user = call.receive<UserCredentials>()
+                val user = call.receive<AuthRoutingUtil.UserCredentials>()
 
                 // Add new user
                 val success = userRepository.addUser(user.username, user.password)
@@ -61,15 +41,15 @@ fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudie
                 }
 
                 // Generate tokens for new user user
-                val tokenResponse = generateTokens(
+                val tokenResponse = AuthRoutingUtil.generateTokens(
                     userRow[Users.id],
                     jwtIssuer,
                     jwtAudience,
                     jwtSecret,
-                    Date(accessTokenExpiryDate),
-                    Date(refreshTokenExpiryDate),
+                    Date(AuthRoutingUtil.accessTokenExpiryDate),
+                    Date(AuthRoutingUtil.refreshTokenExpiryDate),
                 )
-                setTokenHeader(call, tokenResponse)
+                AuthRoutingUtil.setTokenHeader(call, tokenResponse)
                 call.respond(HttpStatusCode.Created, tokenResponse)
             } catch (e: Exception) {
                 call.respond(
@@ -83,7 +63,7 @@ fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudie
 
         post("/login") {
             try {
-                val user = call.receive<UserCredentials>()
+                val user = call.receive<AuthRoutingUtil.UserCredentials>()
 
                 // check for username in DB
                 val userRow = userRepository.findUser(user.username)
@@ -106,15 +86,15 @@ fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudie
                 }
 
                 // Generate new access and refresh tokens
-                val tokenResponse = generateTokens(
+                val tokenResponse = AuthRoutingUtil.generateTokens(
                     userRow[Users.id],
                     jwtIssuer,
                     jwtAudience,
                     jwtSecret,
-                    Date(accessTokenExpiryDate),
-                    Date(refreshTokenExpiryDate),
+                    Date(AuthRoutingUtil.accessTokenExpiryDate),
+                    Date(AuthRoutingUtil.refreshTokenExpiryDate),
                 )
-                setTokenHeader(call, tokenResponse)
+                AuthRoutingUtil.setTokenHeader(call, tokenResponse)
                 call.respond(HttpStatusCode.OK, tokenResponse)
             } catch (e: Exception) {
                 call.respond(
@@ -139,7 +119,7 @@ fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudie
                 return@post
             }
 
-            val (accessToken, refreshToken) = TokenResponse(accessTokenCookie, refreshTokenCookie)
+            val (accessToken, refreshToken) = AuthRoutingUtil.TokenResponse(accessTokenCookie, refreshTokenCookie)
 
             if (accessToken.isBlank() || refreshToken.isNullOrBlank()) {
                 call.respond(
@@ -150,7 +130,7 @@ fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudie
             }
 
             // Get user id from the token
-            var userId = decodeTokenToUid(
+            var userId = AuthRoutingUtil.decodeTokenToUid(
                 jwtIssuer,
                 jwtAudience,
                 jwtSecret,
@@ -159,7 +139,7 @@ fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudie
 
             // If access token has expired, use refresh token to create new tokens
             if (userId == null) {
-                userId = decodeTokenToUid(jwtIssuer, jwtAudience, jwtSecret, refreshToken)
+                userId = AuthRoutingUtil.decodeTokenToUid(jwtIssuer, jwtAudience, jwtSecret, refreshToken)
 
                 // If refresh token has expired, return unauthorized
                 if (userId == null) {
@@ -180,16 +160,16 @@ fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudie
                     return@post
                 }
                 // If refresh token is valid, generate new tokens
-                val tokenResponse = generateTokens(
+                val tokenResponse = AuthRoutingUtil.generateTokens(
                     userRow[Users.id],
                     jwtIssuer,
                     jwtAudience,
                     jwtSecret,
-                    Date(accessTokenExpiryDate),
-                    Date(refreshTokenExpiryDate),
+                    Date(AuthRoutingUtil.accessTokenExpiryDate),
+                    Date(AuthRoutingUtil.refreshTokenExpiryDate),
                 )
 
-                setTokenHeader(call, tokenResponse)
+                AuthRoutingUtil.setTokenHeader(call, tokenResponse)
                 call.respond(HttpStatusCode.OK, tokenResponse)
                 return@post
             }
@@ -205,86 +185,18 @@ fun Route.authRoutes(userRepository: UserRepository, jwtIssuer: String, jwtAudie
             }
 
             // Generate new access token, leave refresh token
-            val tokenResponse = generateTokens(
+            val tokenResponse = AuthRoutingUtil.generateTokens(
                 userRow[Users.id],
                 jwtIssuer,
                 jwtAudience,
                 jwtSecret,
-                Date(accessTokenExpiryDate),
+                Date(AuthRoutingUtil.accessTokenExpiryDate),
                 null
             )
-            val tokens = TokenResponse(tokenResponse.accessToken, refreshToken)
-            setTokenHeader(call, tokens)
+            val tokens = AuthRoutingUtil.TokenResponse(tokenResponse.accessToken, refreshToken)
+            AuthRoutingUtil.setTokenHeader(call, tokens)
             call.respond(HttpStatusCode.OK, tokens)
-
         }
-
     }
-}
-
-fun generateTokens(
-    userId: Int?,
-    jwtIssuer: String,
-    jwtAudience: String,
-    jwtSecret: String,
-    expirationDate: Date,
-    refreshExpirationDate: Date?,
-): TokenResponse {
-    val accessToken = JWT.create()
-        .withIssuer(jwtIssuer)
-        .withAudience(jwtAudience)
-        .withClaim(USER_CLAIM, userId)
-        .withExpiresAt(expirationDate)
-        .sign(Algorithm.HMAC256(jwtSecret))
-
-    if (refreshExpirationDate == null) {
-        return TokenResponse(accessToken, null)
-    }
-    val refreshToken = JWT.create()
-        .withIssuer(jwtIssuer)
-        .withAudience(jwtAudience)
-        .withClaim(USER_CLAIM, userId)
-        .withExpiresAt(refreshExpirationDate)
-        .sign(Algorithm.HMAC256(jwtSecret))
-
-    return TokenResponse(accessToken, refreshToken)
-}
-
-fun decodeTokenToUid(
-    jwtIssuer: String,
-    jwtAudience: String,
-    jwtSecret: String,
-    token: String
-): Int? {
-    try {
-        val decodedToken = JWT.require(Algorithm.HMAC256(jwtSecret))
-            .withIssuer(jwtIssuer)
-            .withAudience(jwtAudience)
-            .build()
-            .verify(token)
-        return decodedToken.getClaim(USER_CLAIM).asInt()
-    } catch (e: Exception) {
-        return null
-    }
-
-}
-
-fun setTokenHeader(
-    call: RoutingCall,
-    tokens: TokenResponse,
-) {
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
-    dateFormat.timeZone = TimeZone.getTimeZone("GMT")
-
-    // Create cookie for accessToken
-    val accessTokenCookie =
-        "accessToken=${tokens.accessToken}; Expires=$dateFormat.format(accessTokenExpiryDate); HttpOnly; Max-Age=${oneWeekInMillis / 1000L}; Path=/; SameSite=Lax"
-    call.response.header(HttpHeaders.SetCookie, accessTokenCookie)
-
-    // Create cookie for refreshToken
-    val refreshTokenCookie =
-        "refreshToken=${tokens.refreshToken}; Expires=$dateFormat.format(refreshTokenExpiryDate); HttpOnly; Max-Age=${fourWeeksInMillis / 1000L}; Path=/; SameSite=Lax"
-    call.response.header(HttpHeaders.SetCookie, refreshTokenCookie)
-
 }
 
---
+++ src/main/kotlin/com/vladisc/financial/server/routing/AuthRoutingUtil.kt
@@ -0,0 +1,92 @@
+package com.vladisc.financial.server.routing
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.http.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import java.text.SimpleDateFormat
+import java.util.*
+
+object AuthRoutingUtil {
+    private const val ONE_WEEK_MS = 7 * 24 * 60 * 60 * 1000L
+    private const val FOUR_WEEKS_MS = ONE_WEEK_MS * 4
+    private const val USER_CLAIM = "uid"
+
+    val accessTokenExpiryDate = System.currentTimeMillis() + ONE_WEEK_MS
+    val refreshTokenExpiryDate = System.currentTimeMillis() + FOUR_WEEKS_MS
+
+    @Serializable
+    data class UserCredentials(val username: String, val password: String)
+
+    @Serializable
+    data class TokenResponse(val accessToken: String, val refreshToken: String?)
+
+    fun generateTokens(
+        userId: Int?,
+        jwtIssuer: String,
+        jwtAudience: String,
+        jwtSecret: String,
+        expirationDate: Date,
+        refreshExpirationDate: Date?,
+    ): TokenResponse {
+        val accessToken = JWT.create()
+            .withIssuer(jwtIssuer)
+            .withAudience(jwtAudience)
+            .withClaim(USER_CLAIM, userId)
+            .withExpiresAt(expirationDate)
+            .sign(Algorithm.HMAC256(jwtSecret))
+
+        if (refreshExpirationDate == null) {
+            return TokenResponse(accessToken, null)
+        }
+        val refreshToken = JWT.create()
+            .withIssuer(jwtIssuer)
+            .withAudience(jwtAudience)
+            .withClaim(USER_CLAIM, userId)
+            .withExpiresAt(refreshExpirationDate)
+            .sign(Algorithm.HMAC256(jwtSecret))
+
+        return TokenResponse(accessToken, refreshToken)
+    }
+
+    fun decodeTokenToUid(
+        jwtIssuer: String,
+        jwtAudience: String,
+        jwtSecret: String,
+        token: String
+    ): Int? {
+        try {
+            val decodedToken = JWT.require(Algorithm.HMAC256(jwtSecret))
+                .withIssuer(jwtIssuer)
+                .withAudience(jwtAudience)
+                .build()
+                .verify(token)
+            return decodedToken.getClaim(USER_CLAIM).asInt()
+        } catch (e: Exception) {
+            return null
+        }
+    }
+
+    fun setTokenHeader(
+        call: RoutingCall,
+        tokens: TokenResponse,
+    ) {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
+        dateFormat.timeZone = TimeZone.getTimeZone("GMT")
+
+        // Create cookie for accessToken
+        val accessTokenExp = dateFormat.format(accessTokenExpiryDate)
+        val accessTokenCookie =
+            "accessToken=${tokens.accessToken}; Expires=$accessTokenExp; HttpOnly; Max-Age=$ONE_WEEK_MS / 1000L}; Path=/; SameSite=Lax"
+        call.response.header(HttpHeaders.SetCookie, accessTokenCookie)
+
+        // Create cookie for refreshToken
+        val refreshTokenExp = dateFormat.format(refreshTokenExpiryDate)
+        val refreshTokenCookie =
+            "refreshToken=${tokens.refreshToken}; Expires=$refreshTokenExp; HttpOnly; Max-Age=${FOUR_WEEKS_MS / 1000L}; Path=/; SameSite=Lax"
+        call.response.header(HttpHeaders.SetCookie, refreshTokenCookie)
+    }
+
+}
